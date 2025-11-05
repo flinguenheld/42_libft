f5c70d4088b7fa4c61e381ffd2d24f40a8485218
Florent Linguenheld <flinguen@c2r6p9.42perpignan.fr>
2025-11-05 19:06:00 +0100

    Add isalpha & isdigit

---
+++ main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flinguen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 15:31:17 by flinguen          #+#    #+#             */
+/*   Updated: 2025/11/05 17:45:50 by flinguen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *print_bool(int val)
+{
+	if (val)
+		return "true";
+	else
+		return "false";
+}
+
+int main(void)
+{
+	for (int i=0; i<255; i++)
+	{
+		if (ft_isalpha(i))
+			printf("%c is alpha\n", i);
+	}
+	for (int i=0; i<255; i++)
+	{
+		if (ft_isdigit(i))
+			printf("%c is digit\n", i);
+	}
+
+	return 0;
+}
+
