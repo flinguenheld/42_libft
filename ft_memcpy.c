a7258b5e4895bfb8054bc21be0dbe2ad16797e5a
flinguen <florent@linguenheld.net>
2025-11-13 15:32:00 +0100

    Refactor memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,34 +6,28 @@
 /*   By: flinguen <florent@linguenheld.net>          +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 13:33:19 by flinguen          #+#    #+#             */
-/*   Updated: 2025/11/10 14:06:46 by flinguen         ###   ########.fr       */
+/*   Updated: 2025/11/13 15:31:48 by flinguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
 
 /*
 DESCRIPTION
 The  memcpy() function copies n bytes from memory area src to memory area dest.
 The memory areas must not overlap. Use memmove if the memory areas do overlap.
 RETURN VALUE
-The memcpy() function returns a pointer to dest.\n
+The memcpy() function returns a pointer to dest.
 */
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char	*d;
-	char	*s;
+	unsigned char	*copied_dest;
 
 	if (dest == NULL && src == NULL)
 		return (NULL);
-	d = (char *)dest;
-	s = (char *)src;
-	while (n)
-	{
-		*d = *s;
-		d++;
-		s++;
-		n--;
-	}
+	copied_dest = (unsigned char *)dest;
+	while (n--)
+		*copied_dest++ = *(unsigned char *)src++;
 	return (dest);
 }
