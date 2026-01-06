8859982463076f8e908ab748f5ef760d8b5a263d
Florent <florent@linguenheld.net>
2026-01-06 01:38:41 +0100

    To - Add atol

--- ft_to/ft_atol.c
+++ ft_to/ft_atol.c
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                       :::      ::::::::    */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flinguen <florent@linguenheld.net>          +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 16:33:26 by flinguen          #+#    #+#             */
-/*   Updated: 2026/01/06 01:37:54 by flinguen         ###   ########.fr       */
+/*   Updated: 2026/01/06 01:37:53 by flinguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-int	ft_atoi(const char *str)
+long	ft_atol(const char *str)
 {
-	return ((int)ft_atol(str));
+	int		is_neg;
+	long	value;
+
+	value = 0;
+	is_neg = 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+		is_neg = (*str++ == '-');
+	while (ft_isdigit(*str))
+		value = value * 10 + (*str++ - '0');
+	if (is_neg)
+		value = -value;
+	return (value);
 }
