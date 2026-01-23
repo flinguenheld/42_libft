bcdb5b43f73d40cb4996060a44afeedf10f18d3f
flinguen <florent@linguenheld.net>
2026-01-23 10:30:43 +0100

    list push_front add check empty node

--- ft_list/ft_lst_push_front.c
+++ ft_list/ft_lst_push_front.c
@@ -6,7 +6,7 @@
 /*   By: flinguen <florent@linguenheld.net>          +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 18:27:10 by flinguen          #+#    #+#             */
-/*   Updated: 2026/01/03 17:16:54 by flinguen         ###   ########.fr       */
+/*   Updated: 2026/01/23 10:30:36 by flinguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	ft_lst_push_front(t_list **lst, t_list *new_node)
 {
-	new_node->next = *lst;
-	(*lst) = new_node;
+	if (new_node != NULL)
+	{
+		new_node->next = *lst;
+		(*lst) = new_node;
+	}
 }
